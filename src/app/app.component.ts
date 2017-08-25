5ddd2f65db6292d18ef635ed4130e5de22a1a078
US\summishra <summishra@deloitte.com>
2017-08-25 12:57:54 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Store, provideStore} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+ count$:Observable<any>;
+  constructor(private store:Store<any>){
+    this.count$ = store.select('counter');
+  }
+  onIncrement():void{
+    console.log('Increment clicked');
+    this.store.dispatch({type:INCREMENT})
+  }
+  onDecrement():void{
+    console.log('Decrement clicked');
+     this.store.dispatch({type:DECREMENT})
+  }
 }
+
+export const counter = (state = 0, action ) => {
+  console.log('sfsfs'+action.type);
+  switch(action.type){
+    case INCREMENT: 
+     return state + 1;
+     case DECREMENT: 
+      return state - 1;
+      default:
+      return state;
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, counter } from './app.component';
+import {StoreModule} from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.provideStore({counter},{counter:0})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+//
